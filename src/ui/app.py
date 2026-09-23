93d8401fd5a72caa0521fdf51bcc66ac02ad636c
Flavio Alves <alveslimneto@gmail.com>
2026-09-22 20:08:48 -0400

    fix: sintaxy error

--- src/ui/app.py
+++ src/ui/app.py
@@ -2,6 +2,6 @@ import flet as ft
 
 def main_ui(page: ft.Page):
     page.title = "CodeRank"
-    page.theme_mode = ft.ThemeMode.Dark
+    page.theme_mode = ft.ThemeMode.DARK
 
-    page.add(ft.Text("Ambiente configurado com sucesso!", size=20, color=ft.colors.GREEN_400))
+    page.add(ft.Text("Ambiente configurado com sucesso!", size=20, color=ft.Colors.GREEN_400))
